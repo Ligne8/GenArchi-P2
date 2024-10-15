d66ed4a0ea522b6c64593b536906ac36f9c3df25
Mathieu MARIN <mathieu.marin@epita.fr>
2024-10-15 15:33:10 +0200

    gamberge

---
+++ scripts/frontend.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+apt-get update
+apt upgrade -y
+
+# Install git
+apt-get install git -y
+
+# Clone the repository
+git clone https://github.com/Ligne8/GenArchi-P2.git app
+
+# Install npm
+apt-get install npm -y
+
+# Install nodejs
+apt-get install nodejs -y
+
+cd app/website/frontend
+
+npm install
