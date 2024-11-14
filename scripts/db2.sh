54489bbca534a8194ac4aecf265ea6d28e915b0c
tom.ami <tom.ami@epita.fr>
2024-11-14 01:02:50 +0100

    gamberge

--- scripts/db2.sh
+++ scripts/db2.sh
@@ -9,9 +9,7 @@ REPL_PASSWORD="votre_mot_de_passe"  # Mot de passe de l'utilisateur de réplicat
 MASTER_HOST="10.0.3.10"  # Adresse IP du serveur principal
 LOGFILE="/tmp/db2.log"
 
-
-
-
+# sudo -u postgres psql -c "SHOW transaction_read_only;"
 
 {
 
@@ -25,8 +23,11 @@ echo "Installing PostgreSQL..."
 apt-get update
 apt-get install -y postgresql postgresql-contrib
 
+#install cron 
+apt-get install cron -y
+
 echo "Waiting for the master host to listen on port 5432..."
-while ! nc -w 3 -z $MASTER_HOST 5432; do   
+while ! pg_isready -h $MASTER_HOST -p 5432; do   
   sleep 1
 done
 echo "Master host is now listening on port 5432."
@@ -37,10 +38,26 @@ systemctl start postgresql
 rm -rf $PGDATA/*
 
 echo "Effectuer une sauvegarde de base depuis le serveur principal..."
-PGPASSWORD=$REPL_PASSWORD pg_basebackup -h $MASTER_HOST -D $PGDATA -U $REPL_USER -v -P --wal-method=stream
-if [ $? -ne 0 ]; then
-    echo "Erreur : la sauvegarde de base a échoué."
-    exit 1
+
+attempt=0
+max_attempts=5
+success=false
+
+while [ $attempt -lt $max_attempts ]; do
+  PGPASSWORD=$REPL_PASSWORD pg_basebackup -h $MASTER_HOST -D $PGDATA -U $REPL_USER -v -P --wal-method=stream
+  if [ $? -eq 0 ]; then
+    success=true
+    echo "Sauvegarde de base réussie. Tentative $(($attempt + 1))/$max_attempts."
+    break
+  else
+    echo "Erreur : la sauvegarde de base a échoué. Tentative $(($attempt + 1))/$max_attempts."
+    attempt=$(($attempt + 1))
+    sleep 5
+  fi
+done
+
+if [ "$success" = false ]; then
+  echo "Erreur : la sauvegarde de base a échoué après $max_attempts tentatives."
 fi
 
 chown -R postgres:postgres /var/lib/postgresql
@@ -70,5 +87,27 @@ systemctl restart postgresql
 
 
 echo "Configuration du serveur secondaire terminée."
+
+echo "setting up cron job for failover"
+
+crontab -l > failover
+echo "* * * * * pg_isready -h 10.0.3.10 -p 5432 || sudo -u postgres pg_ctlcluster 16 main promote" >> failover
+echo "* * * * * sleep 10; pg_isready -h 10.0.3.10 -p 5432 || sudo -u postgres pg_ctlcluster 16 main promote" >> failover
+echo "* * * * * sleep 20; pg_isready -h 10.0.3.10 -p 5432 || sudo -u postgres pg_ctlcluster 16 main promote" >> failover
+echo "* * * * * sleep 30; pg_isready -h 10.0.3.10 -p 5432 || sudo -u postgres pg_ctlcluster 16 main promote" >> failover
+echo "* * * * * sleep 40; pg_isready -h 10.0.3.10 -p 5432 || sudo -u postgres pg_ctlcluster 16 main promote" >> failover
+echo "* * * * * sleep 50; pg_isready -h 10.0.3.10 -p 5432 || sudo -u postgres pg_ctlcluster 16 main promote" >> failover
+crontab failover
+
+# Clean up
+rm failover
+
+# Check if crontab is empty and restore if necessary
+if [ -z "$(crontab -l)" ]; then
+  echo "Crontab is empty, restoring failover jobs..."
+  crontab failover
+else
+  echo "Crontab is not empty, failover jobs already set."
+fi
 
 } >> $LOGFILE 2>&1
